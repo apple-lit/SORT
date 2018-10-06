4b2bb0222515daa923f81323a20aa4a594ad53e6
apple-lit <rintaro.kudo@gmail.com>
2018-10-06 16:12:42 +0900

    数字が表示されるようになりました。 ３の倍数の時のみ右のボタンが動きます

--- SORT/GameViewController.swift
+++ SORT/GameViewController.swift
@@ -13,44 +13,54 @@ class GameViewController: UIViewController {
     
     @IBOutlet var numberLabel: UILabel!
     
-    @IBOutlet var right: UIButton!
-    @IBOutlet var wrong: UIButton!
+//    @IBOutlet var right: UIButton!
+//    @IBOutlet var wrong: UIButton!
     
     var number = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //背景Lottie導入
-        
-        let animationView = LOTAnimationView(name: "gradient_animated_background")
-        animationView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
-        animationView.center = self.view.center
-        animationView.loopAnimation = true
-        animationView.contentMode = .scaleToFill
-        animationView.animationSpeed = 1
-        
-        view.addSubview(animationView)
-        
-        animationView.play()
         
         numberLabel.text = String(number)
         
+       //numberLabelを最前面に
+        self.view.bringSubviewToFront(numberLabel)
+
+        //背景Lottie導入
+        
+//        let animationView = LOTAnimationView(name: "gradient_animated_background")
+//        animationView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height)
+//        animationView.center = self.view.center
+//        animationView.loopAnimation = true
+//        animationView.contentMode = .scaleToFill
+//        animationView.animationSpeed = 1
+//
+//        view.addSubview(animationView)
+//
+//        animationView.play()
+        
+        
+        
     }
     
-    @IBAction func judge(sender: UIButton){
-        //数字の処理
+    @IBAction func right(){
         
-        if (number % 3 == 0 || number % 10 == 3){
-            
-        
-        }else {
-            
+        if (number % 3 == 0 || number % 10 == 0){
+            number = number + 1
             numberLabel.text = String(number)
-           
+            
         }
         
+    }
+    
+    @IBAction func wrong(){
         
+        if(number % 3 != 0 || number % 10 != 0){
+            number  = number + 1
+            numberLabel.text = String(number)
+            
+        }
         
     }
     
