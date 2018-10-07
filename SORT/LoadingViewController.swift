d930535cb9f541f302057f64f726fa380281bc8d
apple-lit <rintaro.kudo@gmail.com>
2018-10-08 03:05:11 +0900

    アイコンの解像度を上げました。 ロード時間を短くしました。

--- SORT/LoadingViewController.swift
+++ SORT/LoadingViewController.swift
@@ -29,7 +29,7 @@ class LoadingViewController: UIViewController {
         
         animationView.play()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             animationView.stop()
             transition()
                 
